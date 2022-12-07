f300e11686ed7221152d68ab087c8d704636f9ba
xiaobo-zhao <334517371@qq.com>
2022-12-07 23:20:09 +0800

    图片上传功能实现

--- src/main/java/com/example/demo/util/Ba64Utils.java
+++ src/main/java/com/example/demo/util/Ba64Utils.java
@@ -1,7 +1,9 @@
 package com.example.demo.util;
 import sun.misc.BASE64Decoder;
 
+import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.OutputStream;
 import java.util.UUID;
 
@@ -22,24 +24,47 @@ public class Ba64Utils {
                     b[i] += 256;
                 }
             }
+
             //图片保存名称1
             String newfilename = System.currentTimeMillis() + UUID.randomUUID().toString().replace(".", "").substring(0, 6) + ".jpg";
             //            String fileLocation = "/static/restaurantRes/";
             //文件访问位置
+            System.out.println("======== 成功获得图片名称===========");
             String filePath = "http://42.193.107.153:8080/file/"
 //                    + fileLocation
                     + newfilename;
             //文件存储位置
+            System.out.println("==========成功获得数据库存储位置===========");
 //            String imgFilePath = "D:/images/" + System.currentTimeMillis() + UUID.randomUUID().toString().replace(".", "").substring(0, 6) + ".jpg";//新生成的图片
-            String imgFilePath = "home/picture" +newfilename;
+            String imgFilePath = "/home/picture/" +newfilename;
+            System.out.println("=========成功获得图片位置为================="+imgFilePath);
+            File file = new File(imgFilePath);
+            System.out.println("==============成功执行了1创建文件============");
+            if(!file.getParentFile().exists()){
+                file.getParentFile().mkdirs();
+                System.out.println("=======成功创建了要写入的路径=====");
 
+            }
+            if(!file.exists()){
+                try {
+                    file.createNewFile();
+                    System.out.println("===========成功创建了一个新的文件=========");
+                } catch (IOException ioException) {
+                    System.out.println("1");
+                    ioException.printStackTrace();
+                }
+            }
+            System.out.println("=========前边都正确=======");
             //文件写入
             OutputStream out = new FileOutputStream(imgFilePath);
+            System.out.println("前边正确了,成功写入");
             out.write(b);
+            System.out.println("");
             out.flush();
             out.close();
             return filePath;
         } catch (Exception e) {
+            e.printStackTrace();
             return null;
         }
     }
